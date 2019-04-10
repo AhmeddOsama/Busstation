16cc133d9fe1c45cafd4196c89d0eb40157cb911
AhmeddOsama <48070449+AhmeddOsama@users.noreply.github.com>
2019-04-10 18:21:34 +0200

    Add files via upload

---
+++ Logout.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Bus_Station;
+
+import java.io.IOException;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+
+public class Logout {
+Stage PrimaryStage=new Stage();
+ public void getStage(Stage PrimaryStage)
+    {
+        this.PrimaryStage=PrimaryStage;
+    }
+    public void changeScene() throws IOException
+    {
+        Parent root = FXMLLoader.load(getClass().getResource("FXML1.fxml"));
+        
+        PrimaryStage.setScene(new Scene(root,600,600));
+        PrimaryStage.show();
+    
+}
+}
