9b1470192c4927ee22c48ab59315fe4702bab592
AhmeddOsama <48070449+AhmeddOsama@users.noreply.github.com>
2019-04-10 18:38:02 +0200

    Add files via upload

---
+++ Internal.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Bus_Station;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class Internal extends Trip {
+
+    public Internal(String destination, int numberOfStops, Time departure, Time arrival, Bus_Station.Driver driver, Bus_Station.Vehicle vehicle) {
+        super(destination, numberOfStops, departure, arrival, driver, vehicle);
+    }
+    public Internal (String Destination,int stops,Time Departure,Time Arrival,Driver driver)
+    {
+         super(Destination,stops,Departure, Arrival,driver);
+    }
+    
+    @Override
+    public void calculatePrice()
+    {
+        
+    }
+    
+}
